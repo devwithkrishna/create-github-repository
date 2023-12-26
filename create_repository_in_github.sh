13e8383f92b3f4b5836592a13c7c051c5b443a74
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2023-12-26 21:17:08 +0530

    fixed json issue

--- create_repository_in_github.sh
+++ create_repository_in_github.sh
@@ -1,6 +1,17 @@
-#! /bin/bash
+#!/bin/bash
 
-#GH_TOKEN --> will be passed from workflow as env variable
+# GH_TOKEN --> will be passed from workflow as an env variable
+ORGANIZATION=$1
+REPOSITORY_NAME=$2
+REPOSITORY_DESCRIPTION=$3
+PRIVATE=$4
+VISIBILITY=$5
+HAS_ISSUES=$6
+HAS_PROJECTS=$7
+HAS_WIKI=$8
+GITIGNORE_TEMPLATE=$9
+LICENSE_TEMPLATE=${10}
+GH_TOKEN=${11}
 
 curl -L \
   -X POST \
@@ -8,4 +19,4 @@ curl -L \
   -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/orgs/$ORGANIZATION/repos \
-  -d '{"name":$REPOSITORY_NAME,"description":$c,"homepage":"https://github.com/"$ORGANIZATION/$REPOSITORY_NAME","private":$VISIBILITY,"has_issues":$HAS_ISSUES,"visibility":$VISIBILITY,"has_projects":$HAS_PROJECTS,"has_wiki":$HAS_WIKI,"gitignore_template":$GITIGNORE_TEMPLATE,"license_template":$LICENSE_TEMPLATE}'
+  -d '{"name":"'"$REPOSITORY_NAME"'","description":"'"$REPOSITORY_DESCRIPTION"'","homepage":"https://github.com/'"$ORGANIZATION/$REPOSITORY_NAME"'","private":'"$PRIVATE"',"has_issues":'"$HAS_ISSUES"',"visibility":"'"$VISIBILITY"'","has_projects":'"$HAS_PROJECTS"',"has_wiki":'"$HAS_WIKI"',"gitignore_template":"'"$GITIGNORE_TEMPLATE"'","license_template":"'"$LICENSE_TEMPLATE"'"}'
